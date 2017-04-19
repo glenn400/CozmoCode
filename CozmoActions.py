96b6b5c45ee2ae2a3dc00563fd0e67ec28b5a44f
glenn400 <mee32100@gmail.com>
2017-04-18 19:13:47 -0700

    Added detailed actions that are ready for testing issues with lifting and composite action will work on it
    Signed-off-by: glenn400 <mee32100@gmail.com>

--- CozmoActions.py
+++ CozmoActions.py
@@ -7,6 +7,11 @@ from cozmo.util import degrees
 # http://cozmosdk.anki.com/docs/generated/cozmo.action.html
 
 class Cozmo_Actions:
+    def __init__(self,speed,d,ang,h):
+        self.speed = speed
+        self.distance = d
+        self.angle = ang
+        self.height = h
     def _movebackward(self, distance, speed, robot):
         # travel backward
         robot.drive_straight(distance_mm(distance), speed_mmps(speed)).wait_for_completed()
@@ -16,14 +21,30 @@ class Cozmo_Actions:
     def _rotate(self, angle, robot):
         # cozmo will rotate at some angle
         robot.turn_in_place(degrees(-angle)).wait_for_completed()
-    #def _liftArms(self,height,speed):
-    def moveAction(self, distance, speed, robot: cozmo.robot.Robot):
+    def moveAction(self,robot: cozmo.robot.Robot):
+
         # cozmo moves a distance forward or backward
-        if distance < 0:
-            self._movebackward(distance, speed, robot)
+        if self.distance < 0:
+            self._movebackward(self.distance, self.speed, robot)
         else:
-            self._moveforward(distance, speed, robot)
-    def turnAction(self, robot: cozmo.robot.Robot, angle):
+            self._moveforward(self.distance, self.speed, robot)
+    def turnAction(self, robot: cozmo.robot.Robot):
         # Cozmo turns right or left
-        self._rotate(angle, robot)
-    #def compositeAction(self):
+        self._rotate(self.angle, robot)
+    def liftAction(self,robot: cozmo.robot.Robot):
+        # lift action
+        self._lift(self.height,robot)
+    def _lift(self,height,robot):
+        robot.set_lift_height(height=height, accel=10.0, max_speed=10.0, duration=0.0, in_parallel=False, num_retries=3)
+
+    def compositeAction(self,robot: cozmo.robot.Robot):
+        robot.start_freeplay_behaviors()
+    def stopcompositeAction(self,robot: cozmo.robot.Robot):
+        robot.stop_freeplay_behaviors()
+
+# testing commands
+c = Cozmo_Actions(100,100,130,0.0)
+#cozmo.run_program(c.moveAction)
+#cozmo.run_program(c.turnAction)
+#cozmo.run_program(c.liftAction)
+cozmo.run_program(c.compositeAction)
