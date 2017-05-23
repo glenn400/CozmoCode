3819d31f97056b6e30eef6bbcdf48922c3650d83
glenn400 <mee32100@gmail.com>
2017-05-23 01:58:07 -0700

    must test tasks to ensure they work correctly      which will be done Tuesday will be creating mission control      will also be done Tuesday
    Signed-off-by: glenn400 <mee32100@gmail.com>

--- TaskPlanner.py
+++ TaskPlanner.py
@@ -3,8 +3,12 @@ The TaskPlanner creates Tasks from DownedPins and empty, non-spare PinLocations
 '''
 
 import Cozmo
-import CozmoTasks
+from CozmoTasks import CozmoTasks
+from CozmoTasks import Cozmo_Actions
+from CozmoTasks import CozmoLocation
+from Pendulum import Pendulum
 
 
 class TaskPlanner:
     def __init__(self):
+        self.taskque = CozmoTasks(0, 0)
